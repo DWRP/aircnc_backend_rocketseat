2ff4384b26044869bc2bd6b3ca15e6098011880e
Douglas Walas <douglas.skd@gmail.com>
2020-02-02 15:50:01 -0300

    final updates and changes

--- src/controllers/BookingController.js
+++ src/controllers/BookingController.js
@@ -11,9 +11,15 @@ module.exports = {
             spot: spot_id,
             date,
         })
-
-        await booking.populate("spot").populate("user").execPopulate();
+   
+        await booking.populate("user").populate("spot").execPopulate();
         
+        const ownerSocket = req.connectedUsers[booking.spot.user];
+
+        if (ownerSocket){
+            req.io.to(ownerSocket).emit('booking_request',booking);
+        }
+
         return res.json(booking);
     }
 
--- src/server.js
+++ src/server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const mongoose = require( 'mongoose');
 const cors = require('cors');
 const path = require('path');
-
+const soketio = require('socket.io');
 const routes = require('./routes');
+const http = require('http');
 
 require('dotenv').config();
 const app = express();
+
+const server = http.Server(app);
+const io = soketio(server);
+
+
 
 DB_USER = process.env.DB_USER;
 DB_PASS = process.env.DB_PASS;
@@ -21,10 +27,27 @@ mongoose.connect(DB_URL,{
     useUnifiedTopology:true,
 });
 
+const connectedUsers = {};
+
+io.on('connection', socket=>{
+    
+    const {user_id} = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+    
+});
+
+app.use((req,res,next)=>{
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
 app.use(cors({origin:'*'}));
 app.use(express.json());
 app.use('/files',express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
 
 
-app.listen(process.env.PORT || 8080);
+server.listen(process.env.PORT || 8080);
